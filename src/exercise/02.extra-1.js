06d9a230aaa25724380d7f1d606947e97ba228cd
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-02-13 19:20:16 +0000

    finish ex02.extra-1

---
+++ src/exercise/02.extra-1.js
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function Toggle(props) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return React.Children.map(props.children, child => {
+    return typeof child.type === 'string'
+      ? child
+      : React.cloneElement(child, {on, toggle})
+  })
+}
+
+const ToggleOn = ({on, children}) => (on ? children : null)
+
+const ToggleOff = ({on, children}) => (!on ? children : null)
+
+const ToggleButton = ({on, toggle}) => {
+  return <Switch on={on} onClick={toggle} />
+}
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <ToggleButton />
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
